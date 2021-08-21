4237eb412be26923203435bd3932bb709a10a572
r2rahul <katyayan.rahul@gmail.com>
2021-08-21 14:39:42 +0530

    updates model analysis

--- run_model_analysis.m
+++ run_model_analysis.m
@@ -1,18 +1,3 @@
 clc;clear;
 disp("Section: Model Training")
 v10analysis1
-
-disp("==========")
-disp("Section: Model Validation")
-
-
-
-disp("==========")
-
-disp("Section: Model Global and Local Sensitivity Analysis")
-
-disp("==========")
-
-disp("Results of the Discussion")
-
-disp("==========")
